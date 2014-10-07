c2b5fc6b15245ea22c1187a52a96c3b38dbaaf33
marcospha@gmail.com <marcospha@gmail.com@f9a0b156-2a76-221f-cae1-2ee78a34a4a0>
2014-10-07 16:43:20 +0000

    Agregado logo, y url de las redes.

--- trunk/themes/hebo/views/layouts/header.php
+++ trunk/themes/hebo/views/layouts/header.php
@@ -7,8 +7,15 @@
   
   <div class="span6">
     <div class="social-icons pull-right clearfix">
-        <div class="" style="text-align:right;"><img src="<?php echo $baseUrl;?>/img/icons/social/facebook.png"  alt="Facebook" /> <img src="<?php echo $baseUrl;?>/img/icons/social/twitter.png"  alt="Twitter" /> <img src="<?php echo $baseUrl;?>/img/icons/social/linkedin.png"  alt="LinkedIn" /> <img src="<?php echo $baseUrl;?>/img/icons/social/google.png"  alt="Google+" /> <img src="<?php echo $baseUrl;?>/img/icons/social/rss.png"  alt="RSS" /></div>
-        <div class="header-text" style="">TOLL FREE: (123) 456 7890</div>
+        <div class="" style="text-align:right;">
+        <?php echo CHtml::link(CHtml::image($baseUrl.'/img/icons/social/facebook.png'),'http://www.facebook.com');?>
+        <?php echo CHtml::link(CHtml::image($baseUrl.'/img/icons/social/twitter.png'),'http://www.twitter.com');?>
+        <?php echo CHtml::link(CHtml::image($baseUrl.'/img/icons/social/linkedin.png'),'http://www.linkedin.com');?>
+        <?php echo CHtml::link(CHtml::image($baseUrl.'/img/icons/social/google.png'),'http://www.google.com');?>
+        <?php echo CHtml::link(CHtml::image($baseUrl.'/img/icons/social/rss.png'),'http://www.rss.com');?>
+        <!--<img src="<?php echo $baseUrl;?>/img/icons/social/facebook.png"  alt="Facebook" /> <img src="<?php echo $baseUrl;?>/img/icons/social/twitter.png"  alt="Twitter" /> <img src="<?php echo $baseUrl;?>/img/icons/social/linkedin.png"  alt="LinkedIn" /> <img src="<?php echo $baseUrl;?>/img/icons/social/google.png"  alt="Google+" /> <img src="<?php echo $baseUrl;?>/img/icons/social/rss.png"  alt="RSS" />-->
+        </div>
+        <div class="header-text" style=""><?php echo Yii::t('texto','TELEFONO GRATUITO'); ?>: (123) 456 7890</div>
     </div><!--/.social-icons -->
    
   </div><!--/.span6 -->
