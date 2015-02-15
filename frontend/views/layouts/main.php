e17893c7184b4c7424ee03173b4f9a3dcb098e66
marcospha@gmail.com <marcospha@gmail.com@f9a0b156-2a76-221f-cae1-2ee78a34a4a0>
2015-02-15 17:44:19 +0000

    Incorporación de la interfaz al index. Agregado assets de la interfaz anterior.

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -17,6 +17,61 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+ <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Página de la empresa Eureka Solutions C.A. registrada en Venezuela">
+    <meta name="author" content="Eureka Solutions - eureksolutions.com">
+    <!---<link rel="shortcut icon" href="ico/favicon.png">-->
+
+    <link rel="apple-touch-icon" sizes="57x57" href="ico/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="ico/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="ico/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="ico/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="ico/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="ico/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="ico/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="ico/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="ico/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="ico/favicon-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="ico/favicon-160x160.png" sizes="160x160">
+    <link rel="icon" type="image/png" href="ico/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="ico/favicon-16x16.png" sizes="16x16">
+    <link rel="icon" type="image/png" href="ico/favicon-32x32.png" sizes="32x32">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link href="css/animate-custom.css" rel="stylesheet">
+
+
+    
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+    
+    <script src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/modernizr.custom.js"></script>
+    
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-56478210-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <?php $this->title = 'Eureka Solutions C.A.'; ?>
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
